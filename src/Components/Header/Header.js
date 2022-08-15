7271c65461623a77e77a01a7fe8ee575be83e9e1
Tusar <tusarahmed141@gmail.com>
2022-08-15 18:13:28 +0600

    Added react icons

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -5,25 +5,34 @@ const Header = () => {
     <header className="header">
       <div className="navbar">
         <nav className="nav">
-          <div className="nav__logo">
-            <img src="logo.png" alt="Website Logo" className="nav__logo-img" />
+          <img src="TextLogo.png" alt="Website Logo" className="nav__logo" />
 
-            <div className="nav__menu">
-              <ul className="nav__list">
-                <li className="nav__item">
-                  <a href="/" className="nav__link">Home</a>
-                </li>
-                <li className="nav__item">
-                  <a href="/" className="nav__link">Coins</a>
-                </li>
-                <li className="nav__item">
-                  <a href="/" className="nav__link">About</a>
-                </li>
-                <li className="nav__item">
-                  <a href="/" className="nav__link">Contact</a>
-                </li>
-              </ul>
-            </div>
+          <div className="nav__menu">
+            <ul className="nav__list">
+              <li className="nav__item">
+                <a href="/" className="nav__link">
+                  Home
+                </a>
+              </li>
+              <li className="nav__item">
+                <a href="/" className="nav__link">
+                  Coins
+                </a>
+              </li>
+              <li className="nav__item">
+                <a href="/" className="nav__link">
+                  About
+                </a>
+              </li>
+              <li className="nav__item">
+                <a href="/" className="nav__link">
+                  Contact
+                </a>
+              </li>
+            </ul>
+          </div>
+
+          <div className="nav__toggle">
           </div>
         </nav>
       </div>
