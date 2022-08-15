3b746cd839a51adedf12e6984d5bfa8598f19c04
Tusar <tusarahmed141@gmail.com>
2022-08-15 21:53:52 +0600

    Navbar design complete on small screen

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,13 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
+import { MdMenu, MdClose } from "react-icons/md";
 
 const Header = () => {
+  const [toggle, setToggle] = useState(true);
   return (
     <header className="header">
       <div className="navbar">
         <nav className="nav">
-          <img src="TextLogo.png" alt="Website Logo" className="nav__logo" />
+          <img src="logo.png" alt="Website Logo" className="nav__logo" />
 
-          <div className="nav__menu">
+          <div className={toggle ? "nav__menu" : "nav__menu right-0"}>
             <ul className="nav__list">
               <li className="nav__item">
                 <a href="/" className="nav__link">
@@ -32,7 +34,12 @@ const Header = () => {
             </ul>
           </div>
 
-          <div className="nav__toggle">
+          <div className="nav__toggle" onClick={() => setToggle(!toggle)}>
+            {toggle ? (
+              <MdMenu className="nav__toggle-icon" />
+            ) : (
+              <MdClose className="nav__toggle-icon" />
+            )}
           </div>
         </nav>
       </div>
