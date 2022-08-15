4043240030ddb01aa2514fffa40cb7fb07a6768d
Tusar <tusarahmed141@gmail.com>
2022-08-15 11:57:12 -0700

    Added site logo

--- src/App.js
+++ src/App.js
@@ -2,9 +2,6 @@ function App() {
   return (
     <>
       <button class="btn btn-info">Info</button>
-      <button class="btn btn-success">Success</button>
-      <button class="btn btn-warning">Warning</button>
-      <button class="btn btn-error">Error</button>
     </>
   );
 }
