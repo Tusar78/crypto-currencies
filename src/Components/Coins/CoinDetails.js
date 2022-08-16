3dabaf9b1f5e5f4c65e4d08a4eba23b69d4a4d2c
Tusar <tusarahmed141@gmail.com>
2022-08-16 18:01:24 +0600

    Coindetails page is complete on mobile device

--- src/Components/Coins/CoinDetails.js
+++ src/Components/Coins/CoinDetails.js
@@ -10,9 +10,9 @@ const CoinDetails = () => {
       .then((res) => res.json())
       .then((data) => SetCoinDetails(data));
   }, [coinId]);
-  
+
   const {
-    image: { large },
+    image,
     name,
     market_cap_rank,
     country_origin,
@@ -24,7 +24,7 @@ const CoinDetails = () => {
     developer_score,
     liquidity_score,
     public_interest_score,
-    description: { en },
+    description,
   } = coinDetails;
 
   return (
@@ -33,13 +33,58 @@ const CoinDetails = () => {
         <div className="coin-details__info">
           <h2 className="coin-details__title">General Info:</h2>
           <div className="coin-details__description">
-            <p className="coin-details__single-info"></p>
+            <p className="coin-details__single-info">Coin Name: {name}</p>
+            <p className="coin-details__single-info">
+              Market Cap Rank: {market_cap_rank}
+            </p>
+            <p className="coin-details__single-info">
+              Origin: {country_origin ? country_origin : "Not-available"}
+            </p>
+            <p className="coin-details__single-info">
+              Contract Address:{" "}
+              {contract_address ? contract_address : "Not-available"}
+            </p>
+            <p className="coin-details__single-info">
+              Hashing Algorithm:{" "}
+              {hashing_algorithm ? hashing_algorithm : "Not-available"}
+            </p>
+            <p className="coin-details__single-info">
+              Genesis Date: {genesis_date ? genesis_date : "Not-available"}
+            </p>
+            <p className="coin-details__single-info">
+              Last Updated: {last_updated ? last_updated : "Not-available"}
+            </p>
+          </div>
+          {/* -------- */}
+          <h2 className="coin-details__title">Scores:</h2>
+          <div className="coin-details__description">
+            <p className="coin-details__single-info">
+              Community Score:{" "}
+              {community_score ? community_score : "Not-available"}
+            </p>
+            <p className="coin-details__single-info">
+              Developer Score:{" "}
+              {developer_score ? developer_score : "Not-available"}
+            </p>
+            <p className="coin-details__single-info">
+              Liquidity Score:{" "}
+              {liquidity_score ? liquidity_score : "Not-available"}
+            </p>
+            <p className="coin-details__single-info">
+              Public Interest Score:
+              {public_interest_score ? public_interest_score : "Not-available"}
+            </p>
           </div>
         </div>
         <div className="coin-details__image">
-          <img src={large} alt={name} className="coin-details__img" />
+          <img src={image?.large} alt={name} className="coin-details__img" />
         </div>
-        <h2>Hello World {coinDetails.symbol} </h2>
+        <div className="coin-details__desc">
+          <h2 className="coin-details__title">Description:</h2>
+          <div className="coin-details__description">
+            <p className="coin-details__single-info" title={description?.en}>{description?.en.substr(0, 600)}...</p>
+          </div>
+        </div>
       </div>
     </section>
   );
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -10,28 +10,52 @@ const Header = () => {
       <div className="navbar">
         <nav className="nav">
           <Link to="/">
-            <img src='logo.png' alt="Website Logo" className="nav__logo" />
+            <img
+              src={
+                "./logo.png"
+                  ? "./logo.png"
+                  : "https://raw.githubusercontent.com/Tusar78/crypto-currencies/main/public/logo.png"
+              }
+              alt="Website Logo"
+              className="nav__logo"
+            />
           </Link>
 
           <div className={toggle ? "nav__menu" : "nav__menu right-0"}>
             <ul className="nav__list">
               <li className="nav__item">
-                <ActiveLink to="/home" className="nav__link"  onClick={() => setToggle(true)}>
+                <ActiveLink
+                  to="/home"
+                  className="nav__link"
+                  onClick={() => setToggle(true)}
+                >
                   Home
                 </ActiveLink>
               </li>
               <li className="nav__item">
-                <ActiveLink to="/coins" className="nav__link"  onClick={() => setToggle(true)}>
+                <ActiveLink
+                  to="/coins"
+                  className="nav__link"
+                  onClick={() => setToggle(true)}
+                >
                   Coins
                 </ActiveLink>
               </li>
               <li className="nav__item">
-                <ActiveLink to="/about" className="nav__link"  onClick={() => setToggle(true)}>
+                <ActiveLink
+                  to="/about"
+                  className="nav__link"
+                  onClick={() => setToggle(true)}
+                >
                   About
                 </ActiveLink>
               </li>
               <li className="nav__item">
-                <ActiveLink to="/contact" className="nav__link"  onClick={() => setToggle(true)}>
+                <ActiveLink
+                  to="/contact"
+                  className="nav__link"
+                  onClick={() => setToggle(true)}
+                >
                   Contact
                 </ActiveLink>
               </li>
