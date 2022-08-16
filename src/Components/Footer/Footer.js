ed9387fa0c4e8681e5a9fbdcb5c07cddb869f4ee
Tusar <tusarahmed141@gmail.com>
2022-08-16 22:19:56 +0600

    footer design complete

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,10 +1,79 @@
-import React from 'react';
+import React from "react";
+import { FaFacebookF, FaTwitter, FaGithub, FaYoutube } from "react-icons/fa";
+// import "./Footer.css";
 
 const Footer = () => {
   return (
-    <div>
-      <p>Hello footer</p>
-    </div>
+    <footer className="custom-footer">
+      <div className="contact-container">
+        <div className="contact">
+          <img src="logo.png" alt="Website logo" className="contact__logo" />
+
+          <div className="contact__info">
+            <p className="contact__address">Kushtia, Khulna, Bangladesh</p>
+            <p className="contact__address">
+              tusar.cse78@gmail.com, (+880)1741-262945, WhatsApp [Tusar]
+            </p>
+          </div>
+
+          <div className="contact__social">
+            <a
+              href="https://facebook.com/tus78ar"
+              target="blank"
+              className="contact__link"
+            >
+              <FaFacebookF className="contact__social-icon" />
+            </a>
+            <a
+              href="https://twitter.com/tusar78"
+              target="blank"
+              className="contact__link"
+            >
+              <FaTwitter className="contact__social-icon" />
+            </a>
+            <a
+              href="https://github.com/tusar78"
+              target="blank"
+              className="contact__link"
+            >
+              <FaGithub className="contact__social-icon" />
+            </a>
+            <a
+              href="https://www.youtube.com/innovation-teach"
+              target="blank"
+              className="contact__link"
+            >
+              <FaYoutube className="contact__social-icon" />
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className="copyright-container">
+        <div className="copyright">
+          <div className="copyright__info">
+            <p className="copyright__text">&copy; All Rights Reserved</p>
+            <p className="copyright__text">
+              <a
+                href="https://facebook.com/tus78ar"
+                target="blank"
+                className="copyright__text-link"
+              >
+                Tusar
+              </a>
+            </p>
+            <p className="copyright__text">
+              <a
+                href="https://facebook.com/innoteach78"
+                target="blank"
+                className="copyright__text-link"
+              >
+                InnovationTeach
+              </a>
+            </p>
+          </div>
+        </div>
+      </div>
+    </footer>
   );
 };
 
