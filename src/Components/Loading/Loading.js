d5a01f1f9fe53372ca475e135a43bb0e1ecd3a50
Tusar <tusarahmed141@gmail.com>
2022-08-17 11:14:04 +0600

    Loading design complete

---
+++ src/Components/Loading/Loading.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Loading = () => {
+  return (
+    <section className='loading'>
+      <img src="loading.gif" alt="loader" />
+    </section>
+  );
+};
+
+export default Loading;
