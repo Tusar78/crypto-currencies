4869b6ebab3920abd9b29aa43d94474f837997cf
Tusar <tusarahmed141@gmail.com>
2022-08-17 11:31:00 +0600

    Added loading

--- src/Components/Coins/CoinDetails.js
+++ src/Components/Coins/CoinDetails.js
@@ -1,14 +1,21 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import Loading from "../Loading/Loading";
 
 const CoinDetails = () => {
   const BASE_URL = "https://api.coingecko.com/api/v3/coins";
   const { coinId } = useParams();
   const [coinDetails, SetCoinDetails] = useState({});
+  const [loading, setLoading] = useState(false);
+
   useEffect(() => {
+    setLoading(true);
     fetch(`${BASE_URL}/${coinId}`)
       .then((res) => res.json())
-      .then((data) => SetCoinDetails(data));
+      .then((data) => {
+        SetCoinDetails(data);
+        setLoading(false);
+      });
   }, [coinId]);
 
   const {
@@ -28,69 +35,86 @@ const CoinDetails = () => {
   } = coinDetails;
 
   return (
-    <section className="section">
-      <div className="coin-details">
-        <div className="coin-details__info">
-          <h2 className="coin-details__title">General Info:</h2>
-          <div className="coin-details__description">
-            <p className="coin-details__single-info">Coin Name: {name}</p>
-            <p className="coin-details__single-info">
-              Market Cap Rank: {market_cap_rank}
-            </p>
-            <p className="coin-details__single-info">
-              Origin: {country_origin ? country_origin : "Not-available"}
-            </p>
-            <p className="coin-details__single-info">
-              Contract Address:{" "}
-              {contract_address ? contract_address : "Not-available"}
-            </p>
-            <p className="coin-details__single-info">
-              Hashing Algorithm:{" "}
-              {hashing_algorithm ? hashing_algorithm : "Not-available"}
-            </p>
-            <p className="coin-details__single-info">
-              Genesis Date: {genesis_date ? genesis_date : "Not-available"}
-            </p>
-            <p className="coin-details__single-info">
-              Last Updated: {last_updated ? last_updated : "Not-available"}
-            </p>
+    <>
+      {loading ? (
+        <Loading />
+      ) : (
+        <section className="section">
+          <div className="coin-details">
+            <div className="coin-details__info">
+              <h2 className="coin-details__title">General Info:</h2>
+              <div className="coin-details__description">
+                <p className="coin-details__single-info">Coin Name: {name}</p>
+                <p className="coin-details__single-info">
+                  Market Cap Rank: {market_cap_rank}
+                </p>
+                <p className="coin-details__single-info">
+                  Origin: {country_origin ? country_origin : "Not-available"}
+                </p>
+                <p className="coin-details__single-info">
+                  Contract Address:{" "}
+                  {contract_address ? contract_address : "Not-available"}
+                </p>
+                <p className="coin-details__single-info">
+                  Hashing Algorithm:{" "}
+                  {hashing_algorithm ? hashing_algorithm : "Not-available"}
+                </p>
+                <p className="coin-details__single-info">
+                  Genesis Date: {genesis_date ? genesis_date : "Not-available"}
+                </p>
+                <p className="coin-details__single-info">
+                  Last Updated: {last_updated ? last_updated : "Not-available"}
+                </p>
+              </div>
+              {/* -------- */}
+              <h2 className="coin-details__title">Scores:</h2>
+              <div className="coin-details__description">
+                <p className="coin-details__single-info">
+                  Community Score:{" "}
+                  {community_score ? community_score : "Not-available"}
+                </p>
+                <p className="coin-details__single-info">
+                  Developer Score:{" "}
+                  {developer_score ? developer_score : "Not-available"}
+                </p>
+                <p className="coin-details__single-info">
+                  Liquidity Score:{" "}
+                  {liquidity_score ? liquidity_score : "Not-available"}
+                </p>
+                <p className="coin-details__single-info">
+                  Public Interest Score:
+                  {public_interest_score
+                    ? public_interest_score
+                    : "Not-available"}
+                </p>
+              </div>
+            </div>
+            <div className="coin-details__image">
+              <img
+                src={image?.large}
+                alt={name}
+                className="coin-details__img"
+              />
+            </div>
           </div>
-          {/* -------- */}
-          <h2 className="coin-details__title">Scores:</h2>
-          <div className="coin-details__description">
-            <p className="coin-details__single-info">
-              Community Score:{" "}
-              {community_score ? community_score : "Not-available"}
-            </p>
-            <p className="coin-details__single-info">
-              Developer Score:{" "}
-              {developer_score ? developer_score : "Not-available"}
-            </p>
-            <p className="coin-details__single-info">
-              Liquidity Score:{" "}
-              {liquidity_score ? liquidity_score : "Not-available"}
-            </p>
-            <p className="coin-details__single-info">
-              Public Interest Score:
-              {public_interest_score ? public_interest_score : "Not-available"}
-            </p>
+          <div className="coin-details">
+            <div className="coin-details__desc">
+              <h2 className="coin-details__title">Description:</h2>
+              <div className="coin-details__description">
+                <p
+                  className="coin-details__single-info"
+                  title={description?.en.length > 600 ? description?.en : ""}
+                >
+                  {description?.en.length > 600
+                    ? description?.en.substr(0, 600) + "..."
+                    : description?.en}
+                </p>
+              </div>
+            </div>
           </div>
-        </div>
-        <div className="coin-details__image">
-          <img src={image?.large} alt={name} className="coin-details__img" />
-        </div>
-      </div>
-      <div className="coin-details">
-        <div className="coin-details__desc">
-          <h2 className="coin-details__title">Description:</h2>
-          <div className="coin-details__description">
-            <p className="coin-details__single-info" title={description?.en.length > 600 ? description?.en : ''}>
-              {description?.en.length > 600 ? description?.en.substr(0, 600) + '...' : description?.en} 
-            </p>
-          </div>
-        </div>
-      </div>
-    </section>
+        </section>
+      )}
+    </>
   );
 };
 
--- src/Components/Coins/Coins.js
+++ src/Components/Coins/Coins.js
@@ -1,34 +1,46 @@
 import React, { useEffect, useState } from "react";
+import Loading from "../Loading/Loading";
 import Coin from "./Coin";
 
 const Coins = () => {
   const BASE_URL = "https://api.coingecko.com/api/v3/coins";
   const [coins, setCoins] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
+    setLoading(true);
     fetch(
       `${BASE_URL}/markets?vs_currency=usd&order=market_cap_desc&per_page=200&page=1&sparkline=false`
     )
       .then((res) => res.json())
-      .then((data) => setCoins(data));
+      .then((data) => {
+        setCoins(data);
+        setLoading(false);
+      });
   }, []);
   return (
-    <section className="section crypto-section">
-      <div className="crypto-currencies">
-        <div className="crypto__info">
-          <h2 className="crypto__title">All Cryptocurrencies</h2>
-          <p className="crypto__text">
-            A cryptocurrency is a digital or virtual currency that is secured by
-            cryptography.
-          </p>
-        </div>
-        <div className="crypto__container">
-          {
-            coins.map(coin => <Coin key={coin.id} coin={coin} />)
-          }
-        </div>
-      </div>
-    </section>
+    <>
+      {loading ? (
+        <Loading />
+      ) : (
+        <section className="section crypto-section">
+          <div className="crypto-currencies">
+            <div className="crypto__info">
+              <h2 className="crypto__title">All Cryptocurrencies</h2>
+              <p className="crypto__text">
+                A cryptocurrency is a digital or virtual currency that is
+                secured by cryptography.
+              </p>
+            </div>
+            <div className="crypto__container">
+              {coins.map((coin) => (
+                <Coin key={coin.id} coin={coin} />
+              ))}
+            </div>
+          </div>
+        </section>
+      )}
+    </>
   );
 };
 
