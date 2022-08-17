81c4dc94c98ebe1cf352987ab5ac9907c01b1cbf
Tusar <tusarahmed141@gmail.com>
2022-08-17 11:43:29 +0600

    Loading successfully addeded

--- src/Components/Loading/Loading.js
+++ src/Components/Loading/Loading.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React from "react";
+import './Loading.css'
 
 const Loading = () => {
   return (
-    <section className='loading'>
-      <img src="loading.svg" alt="loader" />
+    <section className="loading">
+      <div class="loadingio-spinner-interwind-cajtoddrock">
+        <div class="ldio-eu3t9kdey5v">
+          <div>
+            <div>
+              <div>
+                <div></div>
+              </div>
+            </div>
+            <div>
+              <div>
+                <div></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </section>
   );
 };
