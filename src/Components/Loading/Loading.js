427ad573c059eec03b5db72b8539ac6304262f8e
Tusar <tusarahmed141@gmail.com>
2022-08-17 11:39:02 +0600

    Another spinner added

--- src/Components/Loading/Loading.js
+++ src/Components/Loading/Loading.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Loading = () => {
   return (
     <section className='loading'>
-      <img src="loading.gif" alt="loader" />
+      <img src="loading.svg" alt="loader" />
     </section>
   );
 };
